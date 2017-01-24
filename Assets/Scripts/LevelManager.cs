94db2fdc3ffc13d49667d1c4a61d0bb1702ef684
Kait-PC\Kait <matthewbpelletier@gmail.com>
2017-01-23 21:19:36 -0500

    Made splash screen, added sound (everything sucks lol)

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextSceneAfter;
+
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadNextLevel",autoLoadNextSceneAfter);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel (string scenePath)
+	{
+		SceneManager.LoadScene(scenePath);
+	}
+
+	public void QuitApp(){
+		Application.Quit();
+	}
+
+	public void LoadNextLevel ()
+	{
+		print("Loading Scene");
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	private AudioSource music;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing.");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = false;
+			music.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnEnable ()
+	{
+		SceneManager.sceneLoaded += changeMusicOnLevelLoad;
+	}
+	void OnDisable ()
+	{
+		SceneManager.sceneLoaded -= changeMusicOnLevelLoad;
+	}
+
+	void changeMusicOnLevelLoad( Scene scene, LoadSceneMode mode)
+	{
+		Debug.Log ("Music Player Loaded, " + scene);
+		Debug.Log ("Music Player Loaded Mode, " + mode);
+		music.Stop();
+
+		switch (scene.buildIndex) {
+			case 0:
+				music.clip = startClip;
+				music.loop = false;
+				break;
+			case 1:
+				music.clip = gameClip;
+				music.loop = true;
+				break;
+			case 2:
+				music.clip = endClip;
+				music.loop = false;
+				break;
+		}
+
+
+		music.Play();
+	}
+}
